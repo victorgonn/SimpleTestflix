2c7fe812ed9b68a1a8b02cf70f20a9e65f2590d7
victorgonn@gmail.com <Vi207750>
2019-04-30 01:24:24 -0300

    ajustando autolayout da tela de detalhes

--- SimpleTestflix/View/Detail/DetailViewController.swift
+++ SimpleTestflix/View/Detail/DetailViewController.swift
@@ -13,60 +13,135 @@ public class DetailViewController: UIViewController {
 
     public var movie: Movie?
     
+    let movieLogoImgView : UIImageView = {
+        let imgView = UIImageView()
+        imgView.contentMode = UIView.ContentMode.scaleToFill
+        imgView.clipsToBounds =  true
+        imgView.backgroundColor = .red
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+    
+    let dateTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "nome"
+        textView.textColor = .white
+        textView.backgroundColor = .darkGray
+        textView.font = UIFont.boldSystemFont(ofSize: 16)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    let genderTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "nome"
+        textView.textColor = .white
+        textView.backgroundColor = .darkGray
+        textView.font = UIFont.boldSystemFont(ofSize: 16)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    let nameTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "nome"
+        textView.textColor = .white
+        textView.backgroundColor = .darkGray
+        textView.font = UIFont.boldSystemFont(ofSize: 16)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    let overviewTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "nome"
+        textView.textColor = .white
+        textView.backgroundColor = .darkGray
+        textView.font = UIFont.boldSystemFont(ofSize: 16)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .justified
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        setupNavigation()
         setupView()
     }
     
-    func setupView(){
-        let width = view.frame.width
-        let height = view.frame.height
-        let header = UIView(frame: CGRect(x: 0, y: 0, width: width, height: (height * (9/20) + 20 + 20 + 50)))
+    func setupNavigation(){
+        let navigationBarWidth = (navigationController?.navigationBar.frame.width)!
+        let navigationBarHeigth = (navigationController?.navigationBar.frame.height)!
+        let logo = UIImageView(frame: CGRect(x: 0, y: 0, width: (navigationBarWidth * 0.6), height: navigationBarHeigth * 0.6))
+        logo.image = UIImage(named: "testflix")
+        logo.contentMode = .scaleAspectFit
         
-        let imgView = UIImageView()
-        imgView.backgroundColor = .red
-        header.addSubview(imgView)
+        navigationItem.titleView = logo
+        navigationController?.navigationBar.barTintColor = .black
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+    }
+    
+    func setupView(){
+        view.addSubview(movieLogoImgView)
+        view.addSubview(nameTextView)
+        view.addSubview(dateTextView)
+        view.addSubview(genderTextView)
+        view.addSubview(overviewTextView)
         
         let url = movie!.posterPath != nil ? "https://image.tmdb.org/t/p/w185" + movie!.backDropPath! : ""
         if let imgUrl = URL(string: url) {
-            imgView.load(url: imgUrl)
+            movieLogoImgView.load(url: imgUrl)
         }
         
-        
-        let lbl = UILabel()
-        lbl.text = "Titulo: " + self.movie!.title!
-        lbl.textColor = .white
-        lbl.backgroundColor = .black
-        lbl.font = UIFont.boldSystemFont(ofSize: 14)
-        lbl.textAlignment = .center
-        header.addSubview(lbl)
+        nameTextView.text = movie!.title
         
         let df = DateFormatter()
         df.dateFormat = "dd/MM/YYYY"
         
-        let lblDate = UILabel()
-        lblDate.text = "Lançamento: " + (self.movie!.rDate != nil ? df.string(from: self.movie!.rDate!) : "NA")
-        lblDate.textColor = .white
-        lblDate.backgroundColor = .black
-        lblDate.font = UIFont.boldSystemFont(ofSize: 14)
-        lblDate.textAlignment = .center
-        header.addSubview(lblDate)
+        dateTextView.text = movie!.rDate != nil ? df.string(from: movie!.rDate!) : "NA"
+        genderTextView.text = movie!.rGender != nil ? movie!.rGender!.joined(separator: ", ") : "NA"
+        overviewTextView.text = movie!.overview
         
-        let lblSinopse = UILabel()
-        lblSinopse.text = "Sinopse: " + self.movie!.overview!
-        lblSinopse.textColor = .white
-        lblSinopse.backgroundColor = .black
-        lblSinopse.font = UIFont.boldSystemFont(ofSize: 14)
-        lblSinopse.textAlignment = .justified
-        lblSinopse.lineBreakMode = .byWordWrapping
-        lblSinopse.numberOfLines = 0
-        header.addSubview(lblSinopse)
-        
-        header.constraint(pattern: "H:|[v0]|", views: imgView, lbl, lblDate, lblSinopse)
-        header.constraint(pattern: "V:|-90-[v0][v1(20)][v2][v3]|", views: imgView, lbl, lblDate, lblSinopse)
+        setupLayout()
+    }
     
-        view.addSubview(header)
+    private func setupLayout(){
+        let width = view.frame.width
+        let height = view.frame.height
+        
+        movieLogoImgView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        movieLogoImgView.widthAnchor.constraint(equalToConstant: width).isActive = true
+        movieLogoImgView.heightAnchor.constraint(equalToConstant: (height/2.6)).isActive = true
+        
+        nameTextView.topAnchor.constraint(equalTo: movieLogoImgView.bottomAnchor, constant: 10).isActive = true
+        nameTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        nameTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        dateTextView.topAnchor.constraint(equalTo: nameTextView.bottomAnchor, constant: 10).isActive = true
+        dateTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        dateTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        genderTextView.topAnchor.constraint(equalTo: dateTextView.bottomAnchor, constant: 10).isActive = true
+        genderTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        genderTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        overviewTextView.topAnchor.constraint(equalTo: genderTextView.bottomAnchor, constant: 10).isActive = true
+        overviewTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        overviewTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8).isActive = true
+        
     }
     
 }
